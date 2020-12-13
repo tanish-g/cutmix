6700d7538847f78699cb48fed1e1df2260de880f
Taneesh Gupta <55942819+tanish-g@users.noreply.github.com>
2020-12-13 21:10:15 +0530

    Update utils.py

--- cutmix/utils.py
+++ cutmix/utils.py
@@ -1,42 +1,43 @@
 import numpy as np
 import torch
 from torch.nn.modules.module import Module
-
+from torch.nn import CrossEntropyLoss as cross_entropy
 
 class CutMixCrossEntropyLoss(Module):
-    def __init__(self, size_average=True):
+    def __init__(self, size_average=True,weights=None):
         super().__init__()
         self.size_average = size_average
-
+        self.weights=weights
     def forward(self, input, target):
         if len(target.size()) == 1:
             target = torch.nn.functional.one_hot(target, num_classes=input.size(-1))
             target = target.float().cuda()
-        return cross_entropy(input, target, self.size_average)
+        
+        return cross_entropy(input, target, self.size_average,weights=self.weights)
 
 
-def cross_entropy(input, target, size_average=True):
-    """ Cross entropy that accepts soft targets
-    Args:
-         pred: predictions for neural network
-         targets: targets, can be soft
-         size_average: if false, sum is returned instead of mean
+# def cross_entropy(input, target, size_average=True):
+#     """ Cross entropy that accepts soft targets
+#     Args:
+#          pred: predictions for neural network
+#          targets: targets, can be soft
+#          size_average: if false, sum is returned instead of mean
 
-    Examples::
+#     Examples::
 
-        input = torch.FloatTensor([[1.1, 2.8, 1.3], [1.1, 2.1, 4.8]])
-        input = torch.autograd.Variable(out, requires_grad=True)
+#         input = torch.FloatTensor([[1.1, 2.8, 1.3], [1.1, 2.1, 4.8]])
+#         input = torch.autograd.Variable(out, requires_grad=True)
 
-        target = torch.FloatTensor([[0.05, 0.9, 0.05], [0.05, 0.05, 0.9]])
-        target = torch.autograd.Variable(y1)
-        loss = cross_entropy(input, target)
-        loss.backward()
-    """
-    logsoftmax = torch.nn.LogSoftmax(dim=1)
-    if size_average:
-        return torch.mean(torch.sum(-target * logsoftmax(input), dim=1))
-    else:
-        return torch.sum(torch.sum(-target * logsoftmax(input), dim=1))
+#         target = torch.FloatTensor([[0.05, 0.9, 0.05], [0.05, 0.05, 0.9]])
+#         target = torch.autograd.Variable(y1)
+#         loss = cross_entropy(input, target)
+#         loss.backward()
+#     """
+#     logsoftmax = torch.nn.LogSoftmax(dim=1)
+#     if size_average:
+#         return torch.mean(torch.sum(-target * logsoftmax(input), dim=1))
+#     else:
+#         return torch.sum(torch.sum(-target * logsoftmax(input), dim=1))
 
 
 def onehot(size, target):
